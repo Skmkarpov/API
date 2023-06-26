5a63a0e7c907cef996d985ee66652a9609d0ce39
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2023-06-26 20:36:35 +0300

    Первая итерация

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Иван";
+        post.passport = "1234 № 44444444";
+        post.patronumic = "Петрович";
+        post.phone = "+8 (800)-555-35-55";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+
+        post.birthday.day = 15;
+        post.birthday.month = 5;
+        post.birthday.year = 1964;
+    }
+}
